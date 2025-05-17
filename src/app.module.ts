5dbdd9fea7c071b5804860116023fb73ddb09f8d
namdnh <dangngochoainam0601@gmail.com>
2025-05-17 10:51:11 +0700

    test trigger when pull request

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// Test events trigger when pull request is opened
 @Module({
   imports: [],
   controllers: [AppController],
